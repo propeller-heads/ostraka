5b913c3ca36ee75c51c7c3ac1260b4b2b53e85c9
Louise Poole <louise@datarevenue.com>
2023-07-22 18:43:58 +0200

    Set background colours

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -8,20 +8,24 @@ type Props = {
 
 export default function Layout({ children }: Props) {
 	return (
-		<Menu>
-			<Flex layerStyle={'headerFlex'}>
-				<Image
-					boxSize="3rem"
-					hideBelow={'md'}
-					style={{ width: '199.94px' }}
-					ml={5}
-					src="ostraka.svg"
-					alt="ostraka"
-				/>
-				<CustomSismoConnectButton />
-				<Spacer hideFrom={'md'} />
-				{children}
-			</Flex>
-		</Menu>
+		<Flex
+			flexDirection="row"
+			alignItems="center"
+			justifyContent={'space-between'}
+			background="#02111B"
+			height="100px"
+		>
+			<Image
+				boxSize="3rem"
+				hideBelow={'md'}
+				style={{ width: '199.94px' }}
+				ml={5}
+				src="ostraka.svg"
+				alt="ostraka"
+			/>
+			<CustomSismoConnectButton />
+			<Spacer hideFrom={'md'} />
+			{children}
+		</Flex>
 	)
 }
--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head />
-			<body>
+			<body style={{ background: '#5D737E' }}>
 				<Providers>
 					<Main />
 					<NextScript />
