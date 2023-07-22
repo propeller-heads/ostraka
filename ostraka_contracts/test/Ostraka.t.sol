16cfd69644475cbfd4ca6055717abc9dfe592900
czanella <cfszanella@gmail.com>
2023-07-22 14:16:45 +0100

    WIP Ostraka contract

--- ostraka_contracts/test/Ostraka.t.sol
+++ ostraka_contracts/test/Ostraka.t.sol
@@ -5,20 +5,26 @@ import "forge-std/Test.sol";
 import "../src/Ostraka.sol";
 
 contract OstrakaTest is Test {
-    Counter public counter;
+    Ostraka public ostraka;
 
-    // function setUp() public {
-    //     counter = new Counter();
-    //     counter.setNumber(0);
-    // }
+    function setUp() public {
+        ostraka = new Ostraka();
+    }
 
-    // function testIncrement() public {
-    //     counter.increment();
-    //     assertEq(counter.number(), 1);
-    // }
+    function testAddOptions() public {
+        // Define the options to add
+        string[] memory descriptions = new string[](2);
+        descriptions[0] = "Option 1";
+        descriptions[1] = "Option 2";
 
-    // function testSetNumber(uint256 x) public {
-    //     counter.setNumber(x);
-    //     assertEq(counter.number(), x);
-    // }
+        // Call the addOptions function
+        ostraka.addOptions(descriptions);
+
+        // Retrieve the options and check they match what we added
+        OptionPool memory option0 = ostraka.optionPools(0);
+        OptionPool memory option1 = ostraka.optionPools(1);
+
+        assertEq(option0.description, "Option 1", "Option 0 description does not match");
+        assertEq(option1.description, "Option 2", "Option 1 description does not match");
+    }
 }
